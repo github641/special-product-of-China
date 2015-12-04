d77c683b3d93e689ec038c6252b385e4db90c728
刘志意 <mrliuzy@outlook.com>
2015-12-04 12:33:08 +0800

    special product of China 创建工程

---
+++ special product of China/special product of China/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  special product of China
+//
+//  Created by zhiyiLiu on 15/12/4.
+//  Copyright © 2015年 zhiyiLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ special product of China/special product of China/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  special product of China
+//
+//  Created by zhiyiLiu on 15/12/4.
+//  Copyright © 2015年 zhiyiLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
